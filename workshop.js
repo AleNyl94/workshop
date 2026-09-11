643522daa1ed97d6f770feceaa4550fc656cf32f
Alexander Nylander <alexx_94@hotmail.se>
2026-09-11 13:53:55 +0200

    Simulerat brandlarm

--- workshop.js
+++ workshop.js
@@ -22,6 +22,18 @@ function translateRövarspråk(input) {
   return result;
 }
 
+function translateFromRovarSpråk(input) {
+  const vowels = "aeiouyåäö";
+  let result = '';
+  for (const char of input) {
+    const isLetter = char.toLowerCase();
+    const isVowel = vowels.includes(char);
+    if (isLetter && !isVowel) {
+      result -= 
+    }
+  }
+}
+
 function main() {
 rl.question("Skriv ett ord: ", (input) => {
   const translated = translateRövarspråk(input);
