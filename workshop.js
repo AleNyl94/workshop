777f2c714563ea88eeaca32a4d6eac5e19b67e5d
Robin Sanssi <robin.sanssi@icloud.com>
2026-09-11 11:50:11 +0200

    Begin with rövarspråktranslation

--- workshop.js
+++ workshop.js
@@ -1,1 +1,25 @@
-console.log("Workshopen är igång")
+console.log("Workshopen är igång");
+
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function translateRövarspråk(word) {
+  const vowels = "aeiouyåäö";
+  const result = "";
+  for (const char of word) {
+    const isLetter = char;
+    const isVowel = vowels.includes(char);
+    if (isLetter && !isVowel) {
+      result += char + "o" + char;
+    } else {
+      result += char;
+    }
+  }
+  return result;
+}
+
+rl.question("Skriv ett ord: ", (word) => {});
